b3e10002b6e68ea7a481fc5ee8d27fc3950c7abc
rohitshukla12 <rohit.shukla@silverpush.co>
2017-09-29 16:36:06 +0530

    change in rohit

--- server.js
+++ server.js
@@ -2,4 +2,5 @@
 var mailFunc = require('./services/mail');
 
 var message = '<p style="color:red">This is HTML content</p>';
+//Call Mail function
 mailFunc.sendMail('rohit.shukla@silverpush.co','Welcome to ExpertPHP.in',message);
